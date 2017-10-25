16271ca9e720cd84e075adc12ee461e9114cefb6
NasserHumood <nooh43@live.com>
2017-10-25 06:36:49 -0600

    Setup the foundation of the script
    1. welcoming the user
    2. saying goodbye
    3. outline the next steps

---
+++ fqdn-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#   ____        _   _  __ _____
+#  / __ \      | | (_)/ _/ ____|
+# | |  | | __ _| |_ _| || (___   ___ _ ____   __
+# | |  | |/ _` | __| |  _\___ \ / _ \ '__\ \ / /
+# | |__| | (_| | |_| | | ____) |  __/ |   \ V /
+#  \___\_\\__,_|\__|_|_||_____/ \___|_|    \_/
+# Copyright 2017-2018 Qatifserv, Inc.
+# Last Change: 25-10-2017
+# fqdn-setup.sh
+# Simple script to setup a fully qualified domain name in linux systems.
+
+# WARNING: Anything not listed in the currently supported systems list is not
+# going to work, despite the fact that you might see code that detects your
+# OS and acts on it.  If it isn't in the list, the code is not complete and
+# will not work as intended. If the OS isn't listed. Don't even bother trying it.
+
+# Welcoming statement
+echo "Welcome to the FQDN fast setup application"
+echo "Please note that this application only works on Centos 7 / RHEL 7."
+echo "QatifServ will not be responsible for any errors or mistake that might happen as a result of running the application."
+
+# Asking the user for his hostname
+
+# Changing the settings to hostname across the system
+
+# Output the results
+
+# Closing statement
+echo "[ Done ] Your hostname:"
+hostname
+echo "Thank you for using this application. Have a wonderful day."
